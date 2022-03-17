e63bf0df68d86883eb571bac190d85a204dfe703
Tamoy <sinclairtamoy@gmail.com>
2022-03-17 00:20:51 -0400

    commit tree f9c5aa03099c57f79a5ded366bdfd141b4ded669

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function saturdayFun(Brunch){
+    console.log("This Saturday, I want to Brunch")
+}
+function saturdayFun(rollerskate){
+    console.log("This Saturday, I want to roller-skate!")
+}
+function saturdayFun(){
+    console.log('This Saturday, I want to bathe my dog!')
+}  
